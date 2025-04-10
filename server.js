294f466dacf014d572ea3fd059785afb2a4ebdc1
Madhacky <psintellisense@gmail.com>
2025-04-10 14:42:11 +0530

    ..

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const foodRoutes = require('./routes/food_routes');
+const foodRoutes = require('./api/food_routes');
 require('dotenv').config();
 
 const app = express();
